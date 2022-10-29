f36af83a3b7e819c731e637d113586b0a794df27
Alex <cutting.net@yandex.ru>
2022-10-29 15:42:29 +0700

    create project

---
+++ app/src/main/java/com/alex_cutnet/shoppinglist/scenes/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.alex_cutnet.shoppinglist.scenes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.alex_cutnet.shoppinglist.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
